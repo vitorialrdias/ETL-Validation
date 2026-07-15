6fd0e98b1e9dd1d1a4931b5f72f1fd717eb88bf3
Vitoria Leticia Ribeiro Dias <vitoria.lribeiro@sapiens.local>
2026-07-15 10:30:32 -0300

    Init dev repository and test CI/CD

--- _test.py
+++ _test.py
@@ -1,25 +1,30 @@
 import pandas as pd
-from unittest.mock import patch, mock_open
+from unittest.mock import patch
 
 # Importe suas funções
-from src.read_tables import read_origin_table
+from src.read_tables import read_tables
 from src.interface import interface
 
 
 
 # Simula o retorno do banco para testar se o código sabe lidar com o DF
-@patch("builtins.open", new_callable=mock_open, read_data='{}')
 @patch("json.load")
-@patch("src.read_origin_table.pd.read_sql")
-@patch("src.read_origin_table.Database") # Precisamos mocar a classe Database também!
-def test_read_origin_table(mock_db, mock_read_sql, mock_json_load, mock_file):
+@patch("src.read_tables.pd.read_sql")
+@patch("src.read_tables.Database") # Precisamos mocar a classe Database também!
+def test_read_tables(mock_db, mock_read_sql, mock_json_load):
     # 1. Ajustamos o JSON para ter a chave 'sql_dw' que seu código pede
     mock_json_load.return_value = {
-        'sql_dw': {
-            'host': 'localhost',
-            'database': 'test_db',
-            'username': 'admin',
-            'password': 'password123'
+        "sql_dw": {
+            "host": "10.0.0.53",
+            "database": "test_db",
+            "username": "admin",
+            "password": "password123"
+        },
+        "sql_dw_52": {
+            "host": "10.0.0.52",
+            "database": "test_db",
+            "username": "admin",
+            "password": "password123"
         }
     }
 
@@ -29,16 +34,31 @@ def test_read_origin_table(mock_db, mock_read_sql, mock_json_load, mock_file):
 
     # 3. Configuramos o retorno do Banco fake
     df_fake = pd.DataFrame({"id": [1], "nome": ["Teste"]})
-    mock_read_sql.return_value = df_fake
+    df_fake = pd.DataFrame({
+        "id": [1],
+        "nome": ["Teste"]
+    })
+
+    mock_read_sql.side_effect = [
+        iter([df_fake]),  # primeira chamada
+        iter([df_fake])   # segunda chamada
+    ]
 
     # Executamos a função
-    query_test = "SELECT * FROM TABELA"
-    df = read_origin_table(query_test)
+    source_table = ['REFERENCE_TABLE_TESTE', 'dw', '52']
+    target_table = ['TARGET_TABLE_TESTE', 'dw', '53']
+    
+    df_source, df_target = read_tables(source_table, target_table)
 
     # Verificações
-    assert df is not None
-    assert not df.empty
-    assert df.iloc[0]["nome"] == "Teste"
+    assert df_source is not None
+    assert df_target is not None
+    
+    assert not df_source.empty
+    assert not df_target.empty
+    
+    assert df_source.iloc[0]["nome"] == "Teste"
+    assert df_target.iloc[0]["nome"] == "Teste"
 
 # 4. Teste da Interface
 # Streamlit é testado via integração:
--- ui_streamlit.py
+++ ui_streamlit.py
@@ -4,6 +4,6 @@ from config.logging import setup_logging
 setup_logging()
 
 if __name__ == "__main__":
-    # executa a interface grafica do streamlit
+    # Run the streamlit graphical interface
     # bash >> streamlit run ui_streamlit.py
     interface()
